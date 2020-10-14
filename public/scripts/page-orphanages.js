fddc76544e4077d6c308493a528178e7a3187b5a
Wallace de Oliveira <559399+wallaceskt@users.noreply.github.com>
2020-10-14 19:27:45 -0300

    Fiz ajustes e inseri javascript na página de orfanatos.

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,41 @@
+// Create map
+const map = L.map('mapid').setView([-3.7749680, -38.5727036], 15)
+
+// Create and add tileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    {
+        //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+).addTo(map)
+
+// Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170, 2]
+})
+
+// Create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Tia Julia<a href="orphanages.html?id=1" class="choose-orpanage"><img src="./public/images/arrow-white.svg"></a>')
+
+// Create and add marker
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(map)
+
+L
+.marker([-3.7749680, -38.5727036], {icon}) // icon: icon
+.addTo(map)
+.bindPopup(popup)
